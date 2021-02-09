fd39edfb12826b1bfde410a1df0bfcf2fcbdd4be
Elena Porcelli <elena@dedecube.com>
2021-02-09 14:59:54 +0100

    Updated colors

--- ios/Classes/FLTImageCropperPlugin.m
+++ ios/Classes/FLTImageCropperPlugin.m
@@ -160,8 +160,7 @@
     }
     if (cancelButtonTitle && [cancelButtonTitle isKindOfClass:[NSString class]]) {
         controller.cancelButtonTitle = cancelButtonTitle;
-        controller.doneButtonTitle = doneButtonTitle;
-        [controller.toolbar.doneTextButton setTitleColor:[UIColor redColor] 
+       [controller.toolbar.cancelTextButton setTitleColor:[UIColor redColor] 
                forState:UIControlStateNormal & 
                         UIControlStateHighlighted & 
                         UIControlStateSelected];
